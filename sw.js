508cdd854accb5b0a1cccab344a198f22cca8d91
sunyanzi948c-cpu <sunyanzi948c@gmail.com>
2026-08-07 11:47:56 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'todo-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// 安裝 Service Worker 並快取基本檔案
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 攔截網路請求，若有快取則優先使用快取
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // 找到快取，直接回傳
+        }
+        return fetch(event.request); // 沒有快取，則透過網路抓取
+      })
+  );
+});
