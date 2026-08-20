9279409a6ad2e81443ad06ecd0ba0d20fd43db26
sunyanzi948c-cpu <sunyanzi948c@gmail.com>
2026-08-20 14:03:29 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -64,7 +64,7 @@ self.addEventListener('notificationclick', (event) => {
 // ==========================================
 
 // 👇 注意：版號升級到 v3，強制手機更新最新的 sw.js
-const CACHE_NAME = 'todo-pwa-v17'; 
+const CACHE_NAME = 'todo-pwa-v19'; 
 const urlsToCache = [
   './',
   './index.html',
